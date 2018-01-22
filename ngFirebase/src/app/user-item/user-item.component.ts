f9e2ca606f7fe29019247ad418e1ba0c641d4f23
Ram Sundaresan <Ramsundaresan@outlook.com>
2018-01-22 01:52:16 -0600

    Fixes

--- ngFirebase/src/app/user-item/user-item.component.ts
+++ ngFirebase/src/app/user-item/user-item.component.ts
@@ -1,19 +1,14 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { ChatService } from '../services/chat.service';
 import { User } from '../models/user.model';
+
 
 @Component({
   selector: 'app-user-item',
   templateUrl: './user-item.component.html',
   styleUrls: ['./user-item.component.css']
 })
-export class UserItemComponent implements OnInit {
-
+export class UserItemComponent {
   @Input() user: User;
-
   constructor() { }
-
-  ngOnInit() {
-  }
-
 }
